f6ab8aa3d61fb1c33cfa7207c64caf7556af26c2
Yu-Huan Wu <wuyuhuan@mail.nankai.edu.cn>
2022-12-16 14:51:31 +0800

    Support Any Image Size

--- p2t.py
+++ p2t.py
@@ -134,10 +134,10 @@ class PatchEmbed(nn.Module):
         self.norm = nn.LayerNorm(embed_dim)
 
     def forward(self, x):
-        B, C, H, W = x.shape
-        x = self.proj(x).flatten(2).transpose(1, 2)
+        x = self.proj(x)
+        _, _, H, W = x.shape 
+        x = x.flatten(2).transpose(1, 2)
         x = self.norm(x)
-        H, W = H // self.patch_size[0], W // self.patch_size[1]
 
         return x, (H, W)
 
